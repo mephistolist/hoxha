03885f9c43674d1a038415f363e511d97416a5bc
mephistolist <49227141+mephistolist@users.noreply.github.com>
2025-06-12 23:42:59 +0000

    Update anti_debug.c

--- anti_debug.c
+++ anti_debug.c
@@ -68,7 +68,7 @@ static void install_seccomp_ptrace_kill(void) {
         perror("prctl(SECCOMP)");
 }
 
-// 🔒 Call this once early in your program
+// Call this once early in your program
 void anti_debug(void) {
 #ifdef __linux__
     check_tracer_pid();
